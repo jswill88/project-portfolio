a5ed35b887a9007783512d2909b9813ae56c7b80
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-28 02:49:49 -0800

    set up header and make initial color scheme

---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+
+export default function AboutMe () {
+  return (
+    <div id="about-me">
+    <h3>About Me</h3>
+    <p>Lorem Ipsum</p>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import Switch from '@material-ui/core/Switch';
+
+
+export default function Header({ setTheme }) {
+  let url = '#'
+  return (
+    <header>
+      <input type="checkbox" id="menu" />
+      <label htmlFor="menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </label>
+      <nav>
+        <ul>
+          <li><a href="about-me">About</a></li>
+          <li><a href={url}>Projects</a></li>
+          <li><a href={url}>Contact</a></li>
+          <li>
+            <span>Change Theme: </span>
+            <Switch
+              color="default"
+              onChange={() => setTheme(theme => !theme)}
+            />
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Josh Williams<div></div></h1>
+        <h2>Software Developer</h2>
+      </div>
+    </header>
+  )
+}
+
