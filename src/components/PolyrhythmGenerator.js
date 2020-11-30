37d6493332ec14aa0fd2e4a5cc227a1ba53d47c0
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-11-29 16:25:48 -0800

    make body responsive down to 675 pixels

--- src/components/PolyrhythmGenerator.js
+++ src/components/PolyrhythmGenerator.js
@@ -5,7 +5,7 @@ export default function PolyrhythmGenerator() {
   return (
     <div className="project">
       <p>This web application allows a user to hear any polyrhythm. A polyrhythm occurs when different sets of beats are played against one another such as "2 against 3."  It is a skill that advanced musicians work on. I built this app to give musicians a chance to hear complex polyrhythms, so they can internalize the sounds quickly and master them faster. The user can change the tempo, volume, notes, and rhythms.  It is built with Tone.js, React, and SASS.</p>
-      <div>
+      <div id="polyrhythmGeneratorImages">
         <img src={desktopImage} alt="Polyrhythm Generator Demo" />
         <img src={mobileImage} alt="Polyrhythm Mobile" />
       </div>
