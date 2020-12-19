227ec327ab6e15cf2089216a018194e2648ee0ee
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-12-18 23:26:18 -0800

    email form styled for large screen

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,32 @@
 import BackToTop from './BackToTop';
 import Email from './Email'
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Contact() {
   return (
-    <div id="contact" className="bodySection">
-      <h3>Contact</h3>
-      <p>Follow me on LinkedIn and GitHub. Feel free to shoot me an email if you want to collaborate, need a software developer, or just want to say hi!</p>
-      <ul>
-        <li>Email: <a href="mailto:joshuasrwilliams@gmail.com">joshuasrwilliams@gmail.com</a></li>
-        <li>LinkedIn: <a href="https://www.linkedin.com/in/joshua-s-williams/">linkedin.com/in/joshua-s-williams</a></li>
-        <li>GitHub: <a href="https://github.com/jswill88">github.com/jswill88</a></li>
-      </ul>
-      <Email />
-      <BackToTop />
-    </div>
+    <>
+      <div id="contact" className="bodySection">
+        <h3>Contact</h3>
+        <p>Follow me on LinkedIn and GitHub. Feel free to shoot me an email if you want to collaborate, need a software developer, or just want to say hi!</p>
+        <div id="contactBox">
+          <div>
+
+            <Email />
+          </div>
+          <div>
+            <ul>
+              <li><MailOutlineIcon /> <a href="mailto:joshuasrwilliams@gmail.com">joshuasrwilliams@gmail.com</a></li>
+              <li><LinkedInIcon /> <a href="https://www.linkedin.com/in/joshua-s-williams/">linkedin.com/in/joshua-s-williams</a></li>
+              <li><GitHubIcon /> <a href="https://github.com/jswill88">github.com/jswill88</a></li>
+            </ul>
+          </div>
+
+        </div>
+        <BackToTop />
+      </div>
+    </>
 
   )
 }
